ffd8161e0130f871989ed5294a167ccf6a20241a
Noah Guy <noahriguy@gmail.com>
2017-05-31 19:21:51 -0500

    fix split method to properly split input

--- hamming.rb
+++ hamming.rb
@@ -1,8 +1,8 @@
 class Hamming
 	def self.compute(a, b)
 		distance = 0
-		a_comp = a.split
-		b_comp = b.split
+		a_comp = a.split('')
+		b_comp = b.split('')
 
 		a_comp.each do |letter|
 			index_of_letter = a_comp.index(letter)
