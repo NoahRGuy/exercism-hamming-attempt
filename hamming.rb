8b0ec84b25d724f4e4d0c824d5c671188afdd894
Noah Guy <noahriguy@gmail.com>
2017-05-31 19:16:41 -0500

    implement first iteration, 3 tests passed

---
+++ hamming.rb
@@ -0,0 +1,16 @@
+class Hamming
+	def self.compute(a, b)
+		distance = 0
+		a_comp = a.split
+		b_comp = b.split
+
+		a_comp.each do |letter|
+			index_of_letter = a_comp.index(letter)
+			if !letter.eql? b_comp[index_of_letter]
+				distance += 1
+			end
+		end
+
+		return distance
+	end
+end
